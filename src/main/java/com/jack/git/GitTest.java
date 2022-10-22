d7d8c7ac5719eac5d604a0848b484871627281c3
Jack <137520784@qq.com>
2022-10-22 20:41:53 +0800

    hot fix 分支代码测试合并冲突

--- src/main/java/com/jack/git/GitTest.java
+++ src/main/java/com/jack/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("500000");
         System.out.println("333333");
         System.out.println("hot -fix ");
+        System.out.println("hot -fix 代码 测试冲突合并");
     }
 }
