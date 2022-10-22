987d6a2f26b68993be54714ad1cc911a47cea954
Jack <137520784@qq.com>
2022-10-22 20:02:20 +0800

    第一次测试！

---
+++ src/main/java/com/jack/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.jack.git;
+
+/**
+ * FileName: GitTest
+ * Description:
+ * Author: Jack
+ * Date: 2022.10.22 19:54
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("\"Hello Git\" = " + "Hello Git");
+    }
+}
