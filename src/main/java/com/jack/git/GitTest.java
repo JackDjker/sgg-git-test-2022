8827f9f13d6a542037b124d78180000cf7740da2
Jack <137520784@qq.com>
2022-10-22 20:43:50 +0800

    master代码 提交

--- src/main/java/com/jack/git/GitTest.java
+++ src/main/java/com/jack/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("500000");
         System.out.println("333333");
         System.out.println("hot -fix ");
+        System.out.println("master 分支代码 此处会与hot fix 冲突");
     }
 }
