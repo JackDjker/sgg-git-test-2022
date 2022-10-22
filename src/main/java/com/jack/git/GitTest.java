5c7527cfec80b7feee22158f84683404a5513d0d
Jack <137520784@qq.com>
2022-10-22 20:05:16 +0800

    第二次提交！

--- src/main/java/com/jack/git/GitTest.java
+++ src/main/java/com/jack/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("\"Hello Git\" = " + "Hello Git");
+        System.out.println("500000");
     }
 }
